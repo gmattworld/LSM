1e2e12fd03d1d4d3798a39d75a4ba4bf31fd556b
AFOLABI GMATT MATTHEW <gmattworld@gmail.com>
2019-09-11 17:25:24 +0100

    Update

--- LSM.Engine/Controllers/ManageController.cs
+++ LSM.Engine/Controllers/ManageController.cs
@@ -1,12 +1,19 @@
 ﻿using EmbedIO;
 using EmbedIO.Routing;
 using EmbedIO.WebApi;
+using LSM.Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 
 namespace LSM.Engine.Controllers
 {
     public class ManageController : WebApiController
     {
+
         // Gets all records.
         // This will respond to 
         //     GET /api/manage
@@ -14,11 +21,52 @@ namespace LSM.Engine.Controllers
         public object Get()
         {
             return new JavaScriptSerializer()
-            .Serialize(new
+            .Serialize(DB.contacts);
+        }
+
+        // Gets all records.
+        // This will respond to 
+        //     GET /api/manage
+        [Route(HttpVerbs.Get, "/first")]
+        public object GetFirst()
+        {
+            Contact ct = new Contact();
+            ct = null;
+            if (DB.contacts != null)
             {
-                date = "hihih",
-                value = "ksdjkjfsd"
-            });
+                ct = DB.contacts.FirstOrDefault();
+            }
+            return new JavaScriptSerializer()
+            .Serialize(ct);
+        }
+
+        // Gets all records.
+        // This will respond to 
+        //     GET /api/manage
+        [Route(HttpVerbs.Post, "/data")]
+        public object PostData([FormData] NameValueCollection model)
+        {
+            string name = model.Get(0);
+            string email = model.Get(1);
+            string message = model.Get(2);
+            Contact cl = new Contact()
+            {
+                CreatedDate = DateTime.Now,
+                ID = Guid.NewGuid(),
+                UpdateDate = DateTime.Now,
+
+                Email = name,
+                Message = message,
+                Name = email,
+            };
+            if (DB.contacts == null)
+            {
+                DB.contacts = new List<Contact>();
+            }
+
+            DB.contacts.Add(cl);
+
+            return new JavaScriptSerializer().Serialize(cl);
         }
     }
 }
---
+++ LSM.Engine/Models/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSM.Engine.Models
+{
+    public static class DB
+    {
+        public static List<Contact> contacts { get; set; }
+    }
+}
